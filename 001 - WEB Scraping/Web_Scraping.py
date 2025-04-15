9529d6542d4b7a9df052a6cb5774be1df5dbf724
Ana-Lujan <annacarolinalujan@gmail.com>
2025-04-15 19:37:55 -0300

    Primeros trabajos

---
+++ 001 - WEB Scraping/Web_Scraping.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+from wordcloud import WordCloud
+import numpy as np
+from PIL import Image
+import nltk
+from nltk.corpus import stopwords
+import re
+import matplotlib.pyplot as plt
+from collections import Counter
+import random
